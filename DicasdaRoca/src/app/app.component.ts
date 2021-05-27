5de3771baa41f5cdc85a385e3a2a137d1cedc2cf
WellintonGarcia <wellintongarciaa@gmail.com>
2021-05-27 18:35:34 -0300

    Menu

--- DicasdaRoca/src/app/app.component.ts
+++ DicasdaRoca/src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+import { NavController } from '@ionic/angular';
+
+
+
 @Component({
+
   selector: 'app-root',
+
   templateUrl: 'app.component.html',
+
   styleUrls: ['app.component.scss'],
+
 })
+
 export class AppComponent {
-  constructor() {}
+
+  constructor(public nav: NavController) { }
+
+  abrirPaginaLC() {
+
+    this.nav.navigateForward('lista-culturas')
+
+  }
+
+  abrirPaginaCR() {
+
+    this.nav.navigateForward('culturas-recomendadas')
+
+  }
+
+  abrirPaginaMenu() {
+
+    this.nav.navigateForward('menu')
+
+  }
+
 }
