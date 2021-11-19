5cfee550e3751257f076464a8047fc9cef4df3c1
nataliabaptista <natalia.mendes.baptista@hotmail.com>
2021-11-19 16:24:39 -0300

    Consulta dados BD

--- DicasdaRoca/src/app/acomp-lucros/acomp-lucros.page.ts
+++ DicasdaRoca/src/app/acomp-lucros/acomp-lucros.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-acomp-lucros',
@@ -9,18 +8,42 @@ import { Router } from '@angular/router';
   styleUrls: ['./acomp-lucros.page.scss'],
 })
 export class AcompLucrosPage implements OnInit {
+  despesas: string;
   nomeDespesa: string;
   valorDespesa: number;
+
+  mes: number;
+  ano: number;
+
+  faturamentos: string;
+  nomeFaturamento: string;
+  valorFaturamento: number;
+
 
   constructor(public nav: NavController, private http: HttpClient) {}
   abrirPaginaMC(){
     this.nav.navigateForward('menu-calc');
   }
+  selecaoDesp(){
+    this.despesas = 'despesas';
+  }
+  selecaoFatur(){
+    this.faturamentos = 'faturamentos';
+  }
 
   ngOnInit() {
-    this.http.get('https://api-dicasdaroca.herokuapp.com/find/despesas/soja').subscribe(data => {
+    this.http.get('https://api-dicasdaroca.herokuapp.com/find/'+this.despesas+'/'+this.mes+'/'+this.ano).subscribe(data => {
       this.nomeDespesa = data[0]['nome'],
-      this.valorDespesa = data[0]['valor'];
+      this.valorDespesa = data[0]['valor'],
+      this.mes = data[0]['mes'],
+      this.ano = data[0]['ano'];
+    });
+
+    this.http.get('https://api-dicasdaroca.herokuapp.com/find/'+this.faturamentos+'/'+this.mes+'/'+this.ano).subscribe(data => {
+      this.nomeFaturamento = data[0]['nome'],
+      this.valorFaturamento = data[0]['valor'],
+      this.mes = data[0]['mes'],
+      this.ano = data[0]['ano'];
     });
     }
 
